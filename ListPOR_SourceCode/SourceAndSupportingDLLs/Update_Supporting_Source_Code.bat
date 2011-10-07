36bc1fce0c2e900093d3245c3fe4fc74385dcb45
Matthew Monroe <matthew.monroe@pnnl.gov>
2011-10-07 22:48:45 +0000

    Updated batch files

--- ListPOR_SourceCode/SourceAndSupportingDLLs/Update_Supporting_Source_Code.bat
+++ ListPOR_SourceCode/SourceAndSupportingDLLs/Update_Supporting_Source_Code.bat
@@ -6,6 +6,6 @@ Del SmoothProgressBar_Source*.zip
 @echo off
 echo;
 
-Copy "D:\My Documents\Projects\DataMining\DMS_Managers\PRISM_Class_Library\PRISM_Class_Library_Source*.zip" .
-Copy "D:\My Documents\Projects\DataMining\SharedVBNetRoutines\SharedVBNetRoutines_SourceCode\*.zip" .
-Copy "D:\My Documents\Projects\DataMining\SmoothProgressBar.NET\SmoothProgressBar_SourceCode\*.zip" .
+Copy "F:\My Documents\Projects\DataMining\DMS_Managers\PRISM_Class_Library\PRISM_Class_Library_Source*.zip" .
+Copy "F:\My Documents\Projects\DataMining\SharedVBNetRoutines\SharedVBNetRoutines_SourceCode\*.zip" .
+Copy "F:\My Documents\Projects\DataMining\SmoothProgressBar.NET\SmoothProgressBar_SourceCode\*.zip" .
