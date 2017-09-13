eb897a7823c414bfffdc0ed1b405734a6658bc02
alchemistmatt <matthew.monroe@pnnl.gov>
2017-09-12 23:49:58 -0700

    Add new files

---
+++ clsFileData.vb
@@ -0,0 +1,15 @@
+﻿Public Class clsFileData
+    Public ReadOnly Property Key As String
+    Public ReadOnly Property Value As String
+    Public ReadOnly Property RemainingCols As String
+
+    Public Sub New(strKey As String, strValue As String, strRemainingCols As String)
+        Key = strKey
+        Value = strValue
+        RemainingCols = strRemainingCols
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Key & ": " & Value
+    End Function
+End Class
