763bb8e663457d0d88097e22b95eb5472a188f34
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-04 16:03:22 -0700

    Suppress warnings

---
+++ GlobalSuppressions.vb
@@ -0,0 +1,71 @@
+﻿' This file is used by Code Analysis to maintain SuppressMessage
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given
+' a specific target and scoped to a namespace, type, member, etc.
+
+Imports System.Diagnostics.CodeAnalysis
+
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.chkUseNaturalLogValues_CheckedChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.chkUseSymmetricValues_CheckedChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.cmdBrowseForInputFile_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.cmdBrowseForOutputFile_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.cmdExit_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.cmdStart_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.frmListPOR_Closing(System.Object,System.ComponentModel.CancelEventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.frmListPOR_Load(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mListPOR_ErrorEvent(System.String,System.Exception)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mListPOR_ProgressUpdate(System.String,System.Single)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mListPOR_StatusEvent(System.String)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mListPOR_WarningEvent(System.String)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mnuEditResetToDefaults_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mnuFileExit_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mnuFileSelectInputFile_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mnuFileSelectOutputFile_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mnuHelpAbout_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.mnuHelpOverview_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.txtMinimumFinalDataPointCount_LostFocus(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.frmListPOR.txtMinimumFinalDataPointCount_TextChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.modMain.mListPORClass_DebugEvent(System.String)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.modMain.mListPORClass_ErrorEvent(System.String,System.Exception)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.modMain.mListPORClass_StatusEvent(System.String)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ListPOR.modMain.mListPORClass_WarningEvent(System.String)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.cboConfidenceLevel")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.chkAssumeSorted")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.chkIterateRemoval")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.chkUseNaturalLogValues")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.chkUseSymmetricValues")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.cmdBrowseForInputFile")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.cmdBrowseForOutputFile")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.cmdExit")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.cmdStart")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.fraControls")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.fraFilePaths")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.fraOptions")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.lblInputFilePath")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.lblMinimumFinalDataPointCount")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.lblOutputFilePath")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mListPOR")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuEdit")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuEditResetToDefaults")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuFile")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuFileExit")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuFileSelectInputFile")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuFileSelectOutputFile")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuFileSep1")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuHelp")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuHelpAbout")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuHelpOverview")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.mnuHelpSep1")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.pbarProgress")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.txtInputFilePath")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.txtMinimumFinalDataPointCount")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.txtOutputFilePath")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.frmListPOR.txtStatus")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ListPOR.modMain.mListPORClass")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ListPOR.clsFileData")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ListPOR.clsGrubbsTestOutlierFilter")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ListPOR.clsGrubbsTestOutlierFilter.eclConfidenceLevelConstants")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ListPOR.clsGrubbsTestOutlierFilter.udtThresholdInfoType")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ListPOR.clsListPOR")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ListPOR.clsListPOR.eListPORErrorCodeCodes")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ListPOR.frmListPOR")>
